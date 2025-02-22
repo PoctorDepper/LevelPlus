20d49f924a6ec64750b366d57cef2c69f8937c18
Nathaniel Sunday <nathille333@gmail.com>
2025-02-22 05:35:46 -0600

    Update UIConfig.cs

--- Common/Config/UIConfig.cs
+++ Common/Config/UIConfig.cs
@@ -6,13 +6,24 @@ namespace LevelPlus.Common.Config;
 
 public class UIConfig : ModConfig
 {
+    public static UIConfig Instance { get; private set; }
+    
     public override ConfigScope Mode => ConfigScope.ClientSide;
+    
+    public override void OnLoaded() => Instance = this;
 
     [BackgroundColor(0, 0, 0)]
-    [DefaultValue(typeof(Vector2), "0.25, 0.03")]
+    [SliderColor(0, 0, 0)]
+    [Range(0, 1920f)]
+    [DefaultValue(typeof(Vector2), "480, 35")]
     public Vector2 ExperienceBar { get; set; }
     
     [BackgroundColor(0, 0, 0)]
-    [DefaultValue(typeof(Vector2), "0.10, 0.03")]
+    [SliderColor(0, 0, 0)]
+    [Range(0, 1920f)]
+    [DefaultValue(typeof(Vector2), "35, 200")]
     public Vector2 SpendPanel { get; set; }
+
+    [DefaultValue(true)]
+    public bool PointNotifier { get; set; }
 }
